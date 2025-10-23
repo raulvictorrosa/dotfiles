cd23354690c562d04755cca655608d9ab86a4734
Raul Rosa <ror2lis@bosch.com>
2025-10-23 13:19:57 +0100

    feat(nvim): Update bufferline plugin configuration and dependencies

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -4,10 +4,27 @@ return {
   {
     'akinsho/bufferline.nvim',
     version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
+    dependencies = {
+      'nvim-neo-tree/neo-tree.nvim',
+      'nvim-tree/nvim-web-devicons' },
     config = function()
       vim.opt.termguicolors = true
-      require('bufferline').setup {}
+      require('bufferline').setup {
+        options = {
+          middle_mouse_command = 'bdelete! %d',
+
+          tab_size = 21,
+          diagnostics = 'nvim_lsp',
+          offsets = {
+            {
+              filetype = 'neo-tree',
+              text = 'Explorer',
+              text_align = 'left',
+              highlight = 'Directory',
+            },
+          },
+        },
+      }
     end,
   },
 }
