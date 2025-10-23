2b633b3bf9401c2dc42f05a3d0d1ed863791665e
Raul Rosa <ror2lis@bosch.com>
2025-10-23 15:25:48 +0100

    feat(nvim): Refactor VSCode keymap setup for improved clarity and organization

--- nvim/lua/custom/configs/vscode.lua
+++ nvim/lua/custom/configs/vscode.lua
@@ -10,48 +10,64 @@ setupVsCodeConfigs = function()
   vim.o.showmode = true
   vim.o.laststatus = 0
 
-  local vscode = require('vscode')
+  local vscode = require 'vscode'
   -- Remap folding keys to use VSCode commands
   local keymap = vim.keymap.set
   local opts = { noremap = true, silent = true }
   keymap('n', 'zM', function()
-    vscode.call("editor.foldAll")
+    vscode.call 'editor.foldAll'
   end, opts)
   keymap('n', 'zR', function()
-    vscode.call("editor.unfoldAll")
+    vscode.call 'editor.unfoldAll'
   end, opts)
   keymap('n', 'zc', function()
-    vscode.call("editor.fold")
+    vscode.call 'editor.fold'
   end, opts)
   keymap('n', 'zC', function()
-    vscode.call("editor.foldRecursively")
+    vscode.call 'editor.foldRecursively'
   end, opts)
   keymap('n', 'zo', function()
-    vscode.call("editor.unfold")
+    vscode.call 'editor.unfold'
   end, opts)
   keymap('n', 'zO', function()
-    vscode.call("editor.unfoldRecursively")
+    vscode.call 'editor.unfoldRecursively'
   end, opts)
   keymap('n', 'za', function()
-    vscode.call("editor.toggleFold")
+    vscode.call 'editor.toggleFold'
   end, opts)
 
-  -- Additional VSCode-specific keymaps
-  keymap('n', '<leader>sf', function()
-    vscode.call 'workbench.action.quickOpen'
-  end, { desc = 'Quick Open' })
+  -- Additional VSCode-specific keymaps (with absolute priority - will override telescope and other plugins)
+  local function set_vscode_keymap(mode, lhs, rhs, opts)
+    -- Force override any existing keymap
+    vim.keymap.del(mode, lhs, { silent = true })
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
 
-  -- keymap('n', '<leader>fg', function()
-  --   vscode.call 'workbench.view.search'
-  -- end, { desc = 'Search in files' })
+  -- Use vim.schedule to ensure these keymaps are set after all plugins have loaded
+  vim.schedule(function()
+    -- Safely try to delete existing keymaps and set new ones
+    pcall(set_vscode_keymap, 'n', '<leader>sf', function()
+      vscode.call 'workbench.action.quickOpen'
+    end, { desc = 'Quick Open', noremap = true, silent = true })
 
-  keymap('n', '<leader>s<space>', function()
-    vscode.call 'workbench.action.showAllEditors'
-  end, { desc = 'Show all buffers' })
+    pcall(set_vscode_keymap, 'n', '<leader>sg', function()
+      vscode.call 'workbench.view.search'
+    end, { desc = 'Search in files', noremap = true, silent = true })
 
-  keymap('n', '<leader>sc', function()
-    vscode.call 'workbench.action.showCommands'
-  end, { desc = 'Command palette' })
+    pcall(set_vscode_keymap, 'n', '<leader><space>', function()
+      vscode.call 'workbench.action.showAllEditors'
+    end, { desc = 'Show all buffers', noremap = true, silent = true })
+
+    pcall(set_vscode_keymap, 'n', '<leader>ss', function()
+      vscode.call 'workbench.action.showCommands'
+    end, { desc = 'Command palette', noremap = true, silent = true })
+
+    pcall(set_vscode_keymap, 'n', '<leader>sk', function()
+      vscode.call 'workbench.action.openGlobalKeybindings'
+    end, { desc = 'Open Global Keybindings', noremap = true, silent = true })
+
+    -- Uncomment and modify this if you want to override telescope's file search
+  end)
 end
 
 setupVsCodeConfigs()
