67dd86ed16611a984958fae6645a1028b0c2251c
Raul Rosa <ror2lis@bosch.com>
2025-10-24 19:49:11 +0100

    refactor(nvim): Switched to LazyVim Setup

---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+vim.keymap.set({ "n", "t" }, "<c-S-7>", function()
+  Snacks.terminal(nil, { cwd = LazyVim.root() })
+end, { desc = "Terminal (Root Dir)" })
+
+require("config.custom.vscode")
---
+++ nvim/lua/plugins/snacks.lua
@@ -0,0 +1,40 @@
+return {
+  "folke/snacks.nvim",
+  ---@type snacks.Config
+  opts = {
+    picker = {
+      hidden = true, -- Show hidden files in the picker (e.g., using <leader><leader>)
+      ignored = true, -- Show files ignored by git in the picker
+    },
+    explorer = {
+      files = {
+        hidden = true, -- Show hidden files in the file explorer
+        ignored = true, -- Show files ignored by git in the file explorer
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        local explorer_win = nil
+
+        for _, win in ipairs(vim.api.nvim_list_wins()) do
+          local buf = vim.api.nvim_win_get_buf(win)
+          local ft = vim.bo[buf].filetype
+          if ft == "snacks_picker_list" then
+            explorer_win = win
+            break
+          end
+        end
+
+        if vim.api.nvim_get_current_win() ~= explorer_win and explorer_win then
+          vim.api.nvim_set_current_win(explorer_win)
+        else
+          Snacks.explorer()
+        end
+      end,
+      desc = "Snacks File Explorer",
+    },
+  },
+}
