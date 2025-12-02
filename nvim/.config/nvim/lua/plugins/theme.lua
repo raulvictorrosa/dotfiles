a6db3c9dcf061ed905202f2fc147ccfaf3611c39
Raul Rosa <ror2lis@bosch.com>
2025-12-02 11:18:35 +0000

    feat(theme): Replace dracula-theme with updated theme configuration including tokyonight

---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "Mofiqul/dracula.nvim",
+    priority = 1000,
+  },
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
